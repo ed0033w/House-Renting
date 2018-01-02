538839c9514f789d03d0b542cbc0e67e825342ec
nidhogg5 <nidhogg5@yahoo.com.tw>
2018-01-02 19:28:51 +0800

    routes update

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,53 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('index.html');
-});
+router.get('/', function (req, res, next) {
+	res.render('index')
+})
 
-router.get('/house', function(req, res, next) {
-  res.send('house.html');
-});
+router.get('/elements', function (req, res, next) {
+	res.render('elements')
+})
 
-router.get('/fund', function(req, res, next) {
-  res.send('fund.html');
-});
+router.get('/fund', function (req, res, next) {
+	res.render('fund')
+})
 
-router.get('/post', function(req, res, next) {
-  res.send('post.html');
-});
+router.get('/generic', function (req, res, next) {
+	res.render('generic')
+})
 
-router.get('/test', function(req, res, next) {
-  res.send('test.html');
-});
+router.get('/house', function (req, res, next) {
+	res.render('house')
+})
+
+router.get('/info', function (req, res, next) {
+	res.render('info')
+})
+
+router.get('/info', function (req, res, next) {
+	res.render('info')
+})
+
+router.get('/info2', function (req, res, next) {
+	res.render('info2')
+})
+
+router.get('/login', function (req, res, next) {
+	res.render('login')
+})
+
+router.get('/personal', function (req, res, next) {
+	res.render('personal')
+})
+
+router.get('/post', function (req, res, next) {
+	res.render('post')
+})
+
+router.get('/register', function (req, res, next) {
+	res.render('register')
+})
 
 module.exports = router;
