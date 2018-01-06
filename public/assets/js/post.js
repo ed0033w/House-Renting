c7600f21fd1e013d1e3458e4e23e4a21f62ac31c
nidhogg5 <nidhogg5@yahoo.com.tw>
2018-01-06 22:39:47 +0800

    fix bug

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -3,7 +3,13 @@ $("#test").click(function () {
     console.log($("#post_in-form").serialize())
 
     $.post("lease", $("#post_in-form").serialize(), function (result) {
-
         console.log(result)
+        swal({
+            title: '刊登成功',
+            text: '房屋編號:A2342255\n',
+            type: 'success',
+        }).then(() => {
+            window.location = '/house'
+        })
     })
 })
